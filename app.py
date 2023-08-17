8ac2ad63d4d41b7be6876a78528404cb56868834
anmspro <sakib.anms@gmail.com>
2023-08-17 17:52:14 +0600

    prediction system implemented

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 
 app = Flask(__name__)
 model = load_model('Vgg16_leaf.h5')  # Load your trained model
+# image_path = 'static/images/Health/Health_original_IMG_3232.jpg_0a2d05d3-2090-429e-9b56-6b050d188b73.jpg'
 
 def predict_image(image_path, model):
     img = image.load_img(image_path, target_size=(64, 64))  # Adjust target_size as per your model's input requirements
@@ -13,10 +14,12 @@ def predict_image(image_path, model):
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0  # Normalize the image data
     prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
     return prediction
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    class_names = ['Health', 'Bacterial leaf blight', 'Brown spot', 'Leaf smut']
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
@@ -27,7 +30,9 @@ def index():
             file_path = os.path.join('static', 'images', file.filename)
             file.save(file_path)
             prediction = predict_image(file_path, model)
-            return render_template('index.html', prediction=prediction, image_path=file_path)
+            predicted_class_index = np.argmax(np.array(prediction))
+            predicted_class = class_names[predicted_class_index]
+            return render_template('index.html', prediction=prediction, image_path=file_path, predicted_class=predicted_class)
     return render_template('index.html', prediction=None, image_path=None)
 
 if __name__ == '__main__':
