2c51cab670c87fe9ecf2008358b657fd5a9ef1d7
anmspro <sakib.anms@gmail.com>
2023-08-21 02:04:58 +0600

    core functionality modification with design update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Rice-Leaf-Disease-Prediction-System
 
-Rice Leaf Disease Prediction System
+Rice Leaf Disease Prediction System using Deep Learning, specifically leveraging the blessings of transfer learning.
+
+Run the project:
 
 ```
 pip install requirements.txt
@@ -8,3 +10,7 @@ flask run
 ```
 
 Go to: http://127.0.0.1:5000/
+
+Sample preview of the website:
+
+![website_preview](static/images/website_preview.png)
--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from tensorflow.keras.preprocessing import image
 import os
 
 app = Flask(__name__)
+model_vgg16 = load_model('Vgg16_leaf.h5')
+model_vgg19 = load_model('Vgg19_leaf.h5')
+model_alexnet = load_model('Alexnet_leaf.h5')
 model_mobilenet = load_model('MobileNetv2_leaf.h5')
-# model_vgg16 = load_model('Vgg16_leaf.h5')
 model_resnet = load_model('Resnet_leaf.h5')
 model_inceptionv3 = load_model('Inceptionv3_leaf.h5')
 
@@ -16,14 +18,27 @@ def predict_image(image_path, model):
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0 
     prediction = model.predict(img_array)
-    return prediction
+    predicted_class_index = np.argmax(np.array(prediction))
+    return prediction, predicted_class_index
+
+def predict_image75(image_path, model):
+    img = image.load_img(image_path, target_size=(75, 75))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0 
+    prediction = model.predict(img_array)
+    predicted_class_index = np.argmax(np.array(prediction))
+    return prediction, predicted_class_index
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     models = {
-        'MobileNetV2': model_mobilenet,
         'VGG16': model_vgg16,
-        'ResNet': model_resnet
+        'VGG19': model_vgg19,
+        'AlexNet': model_alexnet,
+        'MobileNetV2': model_mobilenet,
+        'ResNet50': model_resnet,
+        'InceptionV3': model_inceptionv3
     }
     class_names = ['Health', 'Bacterial leaf blight', 'Brown spot', 'Leaf smut']
     predictions = {}
@@ -39,11 +54,15 @@ def index():
             file.save(file_path)
             
             for model_name, model in models.items():
-                prediction = predict_image(file_path, model)
-                predicted_class_index = np.argmax(np.array(prediction))
-                predicted_class = class_names[predicted_class_index]
-                predictions[model_name]= predicted_class
-
+                if model_name != 'InceptionV3':
+                    prediction, predicted_class_index = predict_image(file_path, model)
+                    predicted_class = class_names[predicted_class_index]
+                    predictions[model_name] = {'class': predicted_class, 'confidence': prediction[0][predicted_class_index]}
+                else:
+                    prediction, predicted_class_index = predict_image75(file_path, model)
+                    predicted_class = class_names[predicted_class_index]
+                    predictions[model_name] = {'class': predicted_class, 'confidence': prediction[0][predicted_class_index]}
+            
             return render_template('index.html', predictions=predictions, image_path=file_path)
     return render_template('index.html', predictions=None, image_path=None)
 
